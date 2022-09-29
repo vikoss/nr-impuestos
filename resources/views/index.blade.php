dfd41bdf2c8397029035b5331a86154177e8f09d
vikoss <vikoss.yiss@gmail.com>
2022-09-28 21:48:17 -0500

    add index view for load SPA

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head translate="no">
+    <meta charset="UTF-8">
+    <meta name="google" content="notranslate">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    @vite('resources/css/app.css')
+    <title>Impuestos - Nicolás Romero</title>
+</head>
+<body>
+    <div id="app"></div>
+    @vite('resources/js/app.js')
+</body>
+</html>
