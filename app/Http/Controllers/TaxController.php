cc78461e3bab61fa9666c0574122b367f7579f52
vikoss <vikoss.yiss@gmail.com>
2022-11-02 14:43:13 -0600

    add tax controller

---
+++ app/Http/Controllers/TaxController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tax;
+use App\Imports\TaxImport;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Mappers\TaxMapper;
+
+class TaxController extends Controller
+{
+    public function index()
+    {
+        return Tax::paginate();
+    }
+
+    public function show(Tax $tax)
+    {
+        return $tax;
+    }
+
+    public function validity()
+    {
+        return TaxMapper::addFolio(
+            Tax::whereUuid(request('uuid'))->first(),
+            request('type'),
+        );
+    }
+
+    public function store()
+    {
+        Excel::import(new TaxImport, request()->file('file'));
+
+        return response(['success' => true], 201);
+    }
+
+    public function destroy(Tax $tax)
+    {
+        return $tax->delete();
+    }
+}
