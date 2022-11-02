46b58912ea1d9208fb5ecf2e014bff27e27bab3f
vikoss <vikoss.yiss@gmail.com>
2022-11-02 14:23:52 -0600

    add title bar web component

---
+++ resources/js/components/TitleBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mb-6 mt-5 md:mb-12">
+    <h1 class="text-wine text-3xl font-bold">
+      {{ title }}
+    </h1>
+    <span class="h-0.5 w-20 block bg-wine my-3 sm:mt-4 sm:mb-5" />
+    <h2 class="text-base text-black">
+      {{ subtitle }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleBar',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    subtitle: {
+      type: String,
+      default: '',
+    },
+  }
+}
+</script>
