cd8c32b6f895a68bbeb8971b73e78fe064a9b599
vikoss <vikoss.yiss@gmail.com>
2022-11-02 14:40:58 -0600

    add routes for front-end

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -6,6 +6,41 @@ const routes = [
     name: 'Home',
     component: () => import ('./../views/Home.vue'),
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import ('./../views/Login.vue'),
+  },
+  {
+    path: '/certificaciones',
+    name: 'TaxIndex',
+    component: () => import ('./../views/Taxes/Index.vue'),
+  },
+  {
+    path: '/certificaciones/:tax',
+    name: 'TaxShow',
+    component: () => import ('./../views/Taxes/Show.vue'),
+  },
+  {
+    path: '/importar-excel',
+    name: 'TaxCreate',
+    component: () => import ('./../views/Taxes/Create.vue'),
+  },
+  /*
+    Rutas publicas para visualizar certificacion
+  */
+  // Ruta legacy
+  {
+    path: '/ver/:uuid/:type',
+    name: 'TaxValidityLegacy',
+    component: () => import ('./../views/Taxes/Customer/Validity.vue'),
+  },
+  // Ruta nueva
+  {
+    path: '/:type/:uuid',
+    name: 'TaxValidity',
+    component: () => import ('./../views/Taxes/Customer/Validity.vue'),
+  },
 ]
 
 const router = createRouter({
