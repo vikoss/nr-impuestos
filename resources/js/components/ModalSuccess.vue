217784e3a403d9c535b1eff932a6ee13a19faca8
vikoss <vikoss.yiss@gmail.com>
2022-11-02 14:27:22 -0600

    add modal success web component

---
+++ resources/js/components/ModalSuccess.vue
@@ -0,0 +1,60 @@
+<template>
+  <modal-base :show="show" @closed="closed">
+    <div class="py-12 pb-8">
+      <div class="flex flex-col items-center">
+        <success-svg />
+        <h5 class="text-base mb-14 mt-12 text-center" :style="'max-width: 587px; min-width: ' + minWidth ">
+          {{ message }}
+        </h5>
+      </div>
+      <div class="flex justify-center">
+        <button-base
+          class="max-w-xs ml-2"
+          label="Aceptar"
+          @click="action"
+        />
+      </div>
+    </div>
+  </modal-base>
+</template>
+
+<script>
+import ModalBase from './ModalBase.vue'
+import ButtonBase from './ButtonBase.vue'
+import SuccessSvg from './svg/Success.vue'
+
+export default {
+  components: {
+    ModalBase,
+    ButtonBase,
+    SuccessSvg,
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+    closed: {
+      type: Function,
+      default: () => {},
+    },
+    minWidth: {
+      type: String,
+      default: 'auto',
+    },
+    action: {
+      type: Function,
+      default: () => ({}),
+    },
+  },
+  emits: ['closed'],
+  setup() {
+    const close = () => this.$emit('closed')
+    return { close }
+  },
+}
+</script>
