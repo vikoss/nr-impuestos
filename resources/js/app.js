5bc9ab27069ae4796c6fe03034e534a8b6633e5d
vikoss <vikoss.yiss@gmail.com>
2022-11-03 12:15:52 -0600

    add implementation for router middleware front-end

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { isAuthenticated } from './helpers/localstorage'
+
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'TaxValidityLegacy') return next()
+  if (to.name === 'TaxValidity') return next()
+  if (to.name !== 'Login' && !isAuthenticated()) next({ name: 'Login' })
+  else next()
+})
 
 createApp(App)
   .use(router)
