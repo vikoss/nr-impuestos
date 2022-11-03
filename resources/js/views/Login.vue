2d93355e322c0282012080d39befe05db53cf457
vikoss <vikoss.yiss@gmail.com>
2022-11-03 14:11:02 -0600

    change div to img for logo in login view

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,7 +1,11 @@
 <template>
   <main class="flex flex-wrap min-h-screen">
     <div class="flex-grow py-7 px-6 sm:px-12 bg-wine h-full lg:h-auto text-center md:text-left" style="flex-basis: 540px;">
-      <div class="bg-logo-nr h-24 w-auto bg-no-repeat bg-contain bg-center mb-5 sm:h-28 md:h-44 md:bg-left"></div>
+      <img
+        src="./../assets/images/logo-nr.png"
+        class="h-24 w-auto mx-auto mb-5 sm:h-28 md:h-44 md:ml-0"
+        alt="Logo Nicolás Romero"
+      >
       <h1 class="text-white font-light text-4xl sm:text-6xl lg:absolute lg:bottom-36">
         Bienvenido
         <br> a
