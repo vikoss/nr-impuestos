1093f58cbec8b9ae804c76d17f83b2502ad4e4b9
vikoss <vikoss.yiss@gmail.com>
2024-02-19 11:32:24 -0600

    feat: change bottom position of qr code

--- resources/views/templates/qrCode.blade.php
+++ resources/views/templates/qrCode.blade.php
@@ -11,7 +11,7 @@
             }
             .container {
                 position: absolute;
-                bottom: 130px;
+                bottom: 100px;
                 left: 20px;
                 max-width: 100%;
             }
