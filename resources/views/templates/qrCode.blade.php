c5064d80432f8d9bd12cf4cb01932d3812b6dd1e
vikoss <vikoss.yiss@gmail.com>
2024-04-26 15:27:10 -0600

    feat: resize qr code

--- resources/views/templates/qrCode.blade.php
+++ resources/views/templates/qrCode.blade.php
@@ -11,7 +11,7 @@
             }
             .container {
                 position: absolute;
-                bottom: 80px;
+                bottom: 90px;
                 left: 20px;
                 max-width: 100%;
             }
